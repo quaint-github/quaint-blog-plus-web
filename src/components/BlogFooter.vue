6526f7fbf8f29e05bd27199f96d21d0c746b46f5
quaint <qicong@ilarge.cn>
2020-03-10 12:34:40 +0800

    备案地址修改

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <footer>
-            <p>Designed in 2018 by <a href="/">Quaint. </a> <a href="http://www.miitbeian.gov.cn/">豫ICP备18009565号-1</a></p>
+            <p>Designed in 2018 by <a href="/">Quaint. </a> <a href="http://www.beian.miit.gov.cn">豫ICP备18009565号-1</a></p>
             <div style="width:230px;margin:0 auto;">
                 <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41142202000028"
                    style="display:inline-block;height:20px;line-height:20px;">
